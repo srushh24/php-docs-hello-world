05398f824eba006ae0738c04a33e860aaf1ff1b4
srushh24 <Srushti.Parkhi23@iccs.ac.in>
2025-08-06 11:40:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+$string = "hello WORLD";
+$lowercase = strtolower($string);
+$uppercase = strtoupper($string);
+$ucfirst = ucfirst (strtolower ($string));
+$lcfirst = lcfirst($string);
+$ucwords = ucwords(strtolower($string));
+echo"Original string: $string\n";
+echo"strtolower(): $lowercase\n";
+echo"strtoupper(): $uppercase\n";
+echo"ucfirst(): $ucfirst\n";
+echo"lcfirst(): $lcfirst\n";
+echo"ucwords(): $ucwords\n";
+?>
 
-echo "Hello srushti!";
+
